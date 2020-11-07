0ec3480bf2d9a50b5e96439f57dbdf4303cb6be6
WilsonLe <leanhminh2907@gmail.com>
2020-11-08 02:57:41 +0700

    fix socket logic

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ gameSessions = []
 io.on('connection', function (socket) {
 
     socket.on('findPlayer', async () => {
-        console.log(waitingList);
-        if (waitingList.length != 0 && waitingList[waitingList.length - 1] != socket.id){
+        let id = socket.id
+        removeSessionById(id)
+
+        if (waitingList.length != 0 && waitingList[waitingList.length - 1] != socket.id) {
             let p1Id = socket.id
             let p2Id = waitingList.pop()
             gameSessions.push({
@@ -29,7 +31,7 @@ io.on('connection', function (socket) {
             })
             console.log(gameSessions);
             io.to(p1Id).to(p2Id).emit('playerFound')
-        }else{
+        } else if (waitingList.length == 0) {
             waitingList.push(socket.id)
         }
     });
@@ -38,16 +40,31 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
         console.log(socket.id, 'disconnected.');
         let id = socket.id
-        for (let i = 0; i < gameSessions.length; i++){
-            console.log(gameSessions);
-            let gameSession = gameSessions[i]
-            if (gameSession.p1Id == id || gameSession.p2Id == id){
-                io.to(gameSession.p1Id).to(gameSession.p2Id).emit('playerDisconnect')
-                gameSessions.splice(i, 1)
-            }
-        }
+        removeSessionById(id)
+        removeWaitlistById(id)
     });
 })
 
+function removeSessionById(id){
+    for (let i = 0; i < gameSessions.length; i++) {
+        console.log(gameSessions);
+        let gameSession = gameSessions[i]
+        if (gameSession.p1Id == id) {
+            io.to(gameSession.p2Id).emit('playerDisconnect')
+            gameSessions.splice(i, 1)
+        }else if(gameSession.p2Id == id){
+            io.to(gameSession.p1Id).emit('playerDisconnect')
+            gameSessions.splice(i, 1)
+        }
+    }
+}
+
+function removeWaitlistById(id){
+    for (let i = 0; i < waitingList.length; i++) {
+        if (id == waitingList[i]) {
+            waitingList.splice(i, 1)
+        }
+    }
+}
 http.listen(80);
 
