8adcb88ad85db4bb8706c97373c1e78d0af6fbb7
WilsonLe <leanhminh2907@gmail.com>
2020-11-08 09:38:31 +0700

    add initial front end

--- public/index.js
+++ public/index.js
@@ -4,26 +4,51 @@ socket.on('id', (data) => { id = data })
 
 class UI {
     init() {
+        this.infoListener()
         this.startListener()
         this.handleFound()
         this.handleDisconnectPlayer()
         this.inGame()
         this.handleResult()
     }
+    infoListener() {
+        const infoButton = document.querySelector('.info-button')
+        const container = document.querySelector('.container')
+        infoButton.addEventListener('mouseover', () => { 
+            container.classList.add('container-info')
+            container.innerHTML = `
+                <h1>Welcome to the live rock paper scissors game</h1>
+                    <ol>
+                        <li>Enable camera to take part in the game!</li>
+                        <li>Press start to find an opponent to rock against!</li>
+                        <li>Just like a real rock paper scissors game, use your hand to play.</li>
+                    </ol>
+                    <div class="illustration">
+                        <i class="fas fa-hand-rock"></i>
+                        <i class="fas fa-hand-paper"></i>
+                        <i class="fas fa-hand-scissors"></i>
+                    </div>
+            `
+        })
+        infoButton.addEventListener('mouseout', () => { 
+            container.classList.remove('container-info')
+            container.innerHTML = ''
+        })
+    }
     startListener() {
         const startButton = document.querySelector('.start-button')
         startButton.addEventListener('click', () => {
             socket.emit('findPlayer')
-            document.querySelector('.container').innerHTML = 'Finding player...'
+            document.querySelector('.container').innerHTML = '<h1>Finding player...</h1>'
         })
     }
     handleFound() {
         socket.on('playerFound', () => {
-            document.querySelector('.container').innerHTML = 'Player found!'
+            document.querySelector('.container').innerHTML = '<h1>Player found!</h1>'
             setTimeout(() => {
-                document.querySelector('.container').innerHTML = 'Starting game...'
+                document.querySelector('.container').innerHTML = '<h1>Starting game...</h1>'
                 socket.emit('startGame')
-            }, 1000)
+            }, 500)
         })
     }
     inGame() {
